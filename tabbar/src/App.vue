ad6d3f117009b7f73534691494ed3d9e3f848dd0
SQSora <2200960012@qq.com>
2021-08-13 11:35:01 +0800

    修改了一些文件

--- tabbar/src/App.vue
+++ tabbar/src/App.vue
@@ -1,6 +1,4 @@
-<!-- 整个项目为 #APP。组件有：tabbar组件，tabbaritem组件
-App要用tabbar时候直接用在tabbaritem组件弄模板插入到tabbar组件
-tabbar组件再插入到app中 
+<!-- 
 tabbar组件，tabbaritem组件设计与结构都已经封装好了，不用再设置。
 程序运行时候。。先使用<tab-bar>，然后在这里面使用<tab-bar-item>。
 为什么tabbar组件里面是空的，因为用的是tabbat的样式！
@@ -10,22 +8,22 @@ tabbar组件，tabbaritem组件设计与结构都已经封装好了，不用再�
         <router-view></router-view>
     <tab-bar>
       <!-- <div>123</div> -->
-      <tab-bar-item link="/home">
+      <tab-bar-item link="/home" activeColor="#FF5777">
         <img slot="item-icon" src=".\assets\img\tabbar\home.svg">
         <img slot="item-icon-active" src=".\assets\img\tabbar\home_active.svg">  
         <div slot="item-text">首页</div>
       </tab-bar-item>
-      <tab-bar-item link="/category">
+      <tab-bar-item link="/category" activeColor="red">
         <img slot="item-icon" src=".\assets\img\tabbar\category.svg">
         <img slot="item-icon-active" src=".\assets\img\tabbar\category_active.svg">
         <div slot="item-text">分类</div>
       </tab-bar-item>
-      <tab-bar-item link="/shopcart">
+      <tab-bar-item link="/shopcart" activeColor="blue">
         <img slot="item-icon" src=".\assets\img\tabbar\shopcart.svg">
         <img slot="item-icon-active" src=".\assets\img\tabbar\shopcart_active.svg">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
-      <tab-bar-item link="/profile">
+      <tab-bar-item link="/profile" activeColor="#FF5777">
         <img slot="item-icon" src=".\assets\img\tabbar\profile.svg">
         <img slot="item-icon-active" src=".\assets\img\tabbar\profile_active.svg">
         <div slot="item-text">我的</div>
--- tabbar/src/components/tabbar/TabBarItem.vue
+++ tabbar/src/components/tabbar/TabBarItem.vue
@@ -2,20 +2,32 @@
   <div class="tab-bar-item" @click="itemclick">
   <div v-if="!isActive"><slot name="item-icon"></slot></div>
   <div v-else><slot name="item-icon-active"></slot></div>
-  <div :class="{active:isActive}"><slot name="item-text"></slot></div>
-  <!-- 插槽替换时候会把class覆盖掉，∴使用要包裹一个div -->
+  <div :style="activeStyle"><slot name="item-text"></slot></div>
+  <!-- 插槽替换时候会把class覆盖掉，所以使用要包裹一个div -->
   </div>
 </template>
 
 <script>
   export default {
     name: "TabBarItem",
-    props:{ //props 接受父组件传值用
-      link:String
+    props:{ //props 接收父组件传值用
+      link:String,
+      activeColor:{
+        type:String,
+        default:'#FF5777'
+      }
     },
     data() {
       return {
-        isActive:false,
+        // isActive:true,
+      }
+    },
+    computed:{
+      isActive(){
+        return this.$route.path.indexOf(this.link) !== -1 //要写大写O
+      },
+      activeStyle(){
+        return this.isActive ? {color:this.activeColor} : {}
       }
     },
     methods: {
@@ -40,8 +52,5 @@
     margin-top: 3px;
     vertical-align: middle;
     margin-bottom: 2px;
-  }
-  .active{
-    color: #FF5777;
   }
 </style>
