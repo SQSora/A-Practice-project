6debd4d232805daa3098c4b3cf79e4fe4a8e1f77
SQSora <2200960012@qq.com>
2021-08-16 17:10:55 +0800

    update new file

---
+++ Vue的父子组件通信/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+<h2>父组件打印：</h2>
+<h2>{{zinum}}</h2>
+<h2>{{massage}}</h2>
+<comments-vue :Zi_num="app_num"/>
+</div>
+  <!-- //es6单标签 -->
+</template>
+
+<script>
+  import commentsVue from './commentsVue.vue'
+  export default {
+  components: { commentsVue },
+    name: 'App',
+    comments:{
+      commentsVue
+    },
+    data() {
+      return {
+        massage : '一二三',
+        app_num : 0
+      }
+    },
+  }
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+</style>
---
+++ Vue的父子组件通信/commentsVue.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+<h2>---我是子组件内容↓---</h2>
+<h2>{{zi_num}}</h2>
+</template>
+
+<script>
+export default {
+    name : comments1Vue,   
+    props:{
+        zi_num:Number
+    }
+}
+</script>
