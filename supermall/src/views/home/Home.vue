8db34c492b9c8f26aa655bdff32e0d9e697504c2
SQSora <2200960012@qq.com>
2021-08-25 11:34:39 +0800

    轮播图加载有时有问题，可能是接口

--- supermall/src/views/home/Home.vue
+++ supermall/src/views/home/Home.vue
@@ -3,27 +3,22 @@
   <nav-bar class="home-nav">
     <div slot="center">购物街</div>
   </nav-bar>
-    <swiper>
-      <swiper-item v-for="item in banners" key="">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+  <home-swiper :banners="banners"/>
   </div>
 </template>
 
 <script>
   import NavBar from '@/components/common/Navbar/Navbar.vue'
+  import HomeSwiper from './Home_Swiper/HomeSwiper.vue'
   import {getHomeMultidata} from 'network/home'
-  import {Swiper, SwiperItem} from '@/components/common/swiper/index.js'
+
 
   export default {
     name: "Home",
+    
     components:{
       NavBar,
-      Swiper,
-      SwiperItem
+      HomeSwiper
     },
     data(){
       return{
